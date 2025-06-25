340795c6dc4b23c6fdb23df9b77d670e4bc4f8bd
Shal Ritvik Sinha <77838259+Shalritvik@users.noreply.github.com>
2025-06-25 16:25:42 -0400

    Create Leetcode1071.cpp

---
+++ c++/Leetcode1071.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1 + str2 != str2 + str1) return "";
+
+        int gcdlength = gcd(str1.length(),str2.length());
+        return str1.substr(0,gcdlength);
+    }
+private:
+    int gcd(int a, int b)
+    {
+        if(b == 0)
+            return a;
+        else
+            return gcd(b,a%b);
+    }
+};
